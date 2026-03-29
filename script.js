ebb938e40a6676b49861cc38956f44a462357a1f
Kiranmai-15 <yellapukiranmaigangotree@gmail.com>
2026-03-29 11:07:55 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+const quiz = [
+    {
+        question: "What is the capital of India?",
+        options: ["Mumbai", "Delhi", "Kolkata"],
+        answer: "Delhi"
+    },
+    {
+        question: "Which is a programming language?",
+        options: ["HTML", "CSS", "JavaScript"],
+        answer: "JavaScript"
+    },
+    {
+        question: "2 + 6 = ?",
+        options: ["6", "8", "10"],
+        answer: "8"
+    }
+];
+
+let index = 0;
+let score = 0;
+let selectedBtn = null;
+
+const nextBtn = document.getElementById("nextBtn");
+
+nextBtn.addEventListener("click", nextQuestion);
+
+function loadQuestion() {
+    const q = quiz[index];
+    document.getElementById("question").innerText = q.question;
+
+    let optionsHTML = "";
+    q.options.forEach(opt => {
+        optionsHTML += `<button onclick="selectAnswer(this, '${opt}')">${opt}</button>`;
+    });
+
+    document.getElementById("options").innerHTML = optionsHTML;
+
+    updateProgress();
+}
+
+function selectAnswer(btn, ans) {
+    if (selectedBtn) selectedBtn.classList.remove("selected");
+    btn.classList.add("selected");
+    selectedBtn = btn;
+    selectedBtn.value = ans;
+}
+
+function nextQuestion() {
+    if (selectedBtn && selectedBtn.value === quiz[index].answer) {
+        score++;
+    }
+
+    index++;
+    selectedBtn = null;
+
+    if (index < quiz.length) {
+        loadQuestion();
+    } else {
+        document.getElementById("question").innerText = "🎉 Quiz Completed!";
+        document.getElementById("options").innerHTML = "";
+        document.getElementById("score").innerText = "Your Score: " + score + "/" + quiz.length;
+        document.getElementById("progress-bar").style.width = "100%";
+    }
+}
+
+function updateProgress() {
+    let progress = (index / quiz.length) * 100;
+    document.getElementById("progress-bar").style.width = progress + "%";
+}
+
+loadQuestion();
